fae3a51fb5c08e9567d1990e0bb5f7aca7e45c8e
sumit-max-arch <sumitchowdhary01@gmail.com>
2020-08-25 10:41:04 +0530

    changes react to green

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="grid-container">
       <header>
-      <a href ="/">React Shopping Cart</a>
+      <a href ="/">Green Shopping Cart</a>
       </header>
       <main>
         Product List
