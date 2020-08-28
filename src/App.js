eafd20988bbb6db191886a258021f50fb7e22515
sumit-max-arch <sumitchowdhary01@gmail.com>
2020-08-28 10:29:33 +0530

    initial msg

--- src/App.js
+++ src/App.js
@@ -1,21 +1,56 @@
 import React from 'react';
+import data from './data.json';
+import Products from './component/products';
+import Filter from './component/Filter';
 
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: data.products,
+      size: "",
+      sort: "",
+    };
+  }
+  sortProducts(event) {
+    //implements
+    console.log(event.target.value);
+  }
+  filterProducts(event) {
+    //implements
+    console.log(event.target.value);
 
-function App() {
+  }
+  render() {
   return (
-    <div className="grid-container">git
-      <header>
-      <a href ="/">Green fashion </a>
-      </header>
-      <main>
-        Product List
-      </main>
-      <footer>
-        All right is reserved
-      </footer>
-   </div> 
+      <div className="grid-container">
+        <header>
+        <a href="/">Green fashion </a>
+        </header>
+        <main>
+        <div className="content">
+        <div className="main">
+          <Filter
+          count = {this.state.products.length}>
+          size =  {this.state.size}
+          sort =  {this.state.sort}
+          filterProducts = {this.filterProducts}
+          sortProducts = {this.sortProducts}
+          </Filter>
+          <Products products ={this.state.products}></Products>
+          </div>
+          <div className="sidebar">
+              cart item
+          </div>
+          </div>
+          </main>
+         <footer>
+          All right is reserved
+        </footer>
+        </div>
    
-  );
+    );
+  }
 }
 
 export default App;
